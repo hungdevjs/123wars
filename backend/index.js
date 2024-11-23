6a30f07dcc4436c56bac15201e1fdcae92206a0f
jackday1 <jack.nguyen@weareday.one>
2024-11-23 09:49:36 +0700

    deploy to vercel

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,19 @@ import environments from './utils/environments.js';
 
 const { PORT } = environments;
 
-const main = async () => {
-  const app = express();
+const app = express();
 
-  app.use(cors());
-  app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-  app.use(errorHandler);
+app.use(errorHandler);
 
-  app.get('/', (req, res) => {
-    res.send('OK 1.0.62');
-  });
+app.get('/', (req, res) => {
+  res.send('OK 1.0.62');
+});
 
-  app.use('/api', routes);
+app.use('/api', routes);
 
-  app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
-};
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
 
-main();
+export default app;
