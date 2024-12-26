2eb0dbf937da4c85523cb55a4dc15e1697010c41
jackday1 <jack.nguyen@weareday.one>
2024-12-26 14:05:02 +0700

    fix: missing package lsitenrs

--- app/src/configs/firebase.config.js
+++ app/src/configs/firebase.config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 import environments from '../utils/environments';
 
